19a4a20da42e49a8c796a1c191bdce69b22a3e11
nail361 <nail361@mail.ru>
2019-11-16 15:56:45 +0300

    upd. drag

--- projects/src/components/Game1.jsx
+++ projects/src/components/Game1.jsx
@@ -11,38 +11,73 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
+const switchDroppable = (index, oldDrop, newDrop) => {
+  newDrop.push(oldDrop.splice(index, 1));
+
+  return { newIcons: oldDrop, newPlanetIcons: newDrop };
+};
+
+const trueAnswers = ['bird', 'animal', 'people', 'fish'];
+
 class Game1 extends PureComponent {
   constructor(props) {
     super(props);
 
+    let icons = ['bird', 'animal', 'people', 'fish', 'dino', 'nfo'];
+    icons = icons.sort(() => Math.random() - 0.5);
+
     this.state = {
-      icons: ['bird', 'animal', 'people', 'fish', 'dino', 'nfo'],
+      icons,
+      planetIcons: [],
     };
+
+    this.onDragEnd = this.onDragEnd.bind(this);
   }
 
   onDragEnd(result) {
     // dropped outside the list
-    if (!result.destination) {
-      return;
+    console.log(result);
+
+    if (result.destination) {
+      if (result.destination.droppableId === 'planet') {
+        const { icons, planetIcons } = this.state;
+
+        const { newIcons, newPlanetIcons } = switchDroppable(
+          result.source.index,
+          icons,
+          planetIcons,
+        );
+
+        this.setState({
+          icons: newIcons,
+          planetIcons: newPlanetIcons,
+        });
+      } else {
+        const { icons } = this.state;
+
+        const newIcons = reorder(
+          icons,
+          result.source.index,
+          result.destination.index,
+        );
+
+        this.setState({
+          icons: newIcons,
+        });
+      }
     }
-
-    const { icons } = this.state;
-
-    const newIcons = reorder(
-      icons,
-      result.source.index,
-      result.destination.index,
-    );
-
-    this.setState({
-      icons: newIcons,
-    });
   }
 
-  static getIcons(icons) {
+  static getIcons(icons, place) {
+    const isDragDisabled = place === 'planet';
     const iconsDiv = icons.map((icon, index) =>
       (
-        <Draggable key={icon} draggableId={icon} index={index}>
+        <Draggable
+          key={icon}
+          draggableId={icon + place}
+          index={index}
+          isDragDisabled={isDragDisabled}
+        >
           {(provided) => (
             <div
               {...provided.draggableProps}
@@ -60,14 +95,25 @@ class Game1 extends PureComponent {
   }
 
   render() {
-    const { icons } = this.state;
+    const { icons, planetIcons } = this.state;
 
     return (
       <div className="game1-wrapper">
         <h2>Выберите кто населяет планету:</h2>
         <h3>Перенесите нужные иконки, из списка, на планету</h3>
-        <div className="planet" />
         <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="planet">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="planet"
+              >
+                {Game1.getIcons(planetIcons, 'planet')}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
           <Droppable droppableId="droppable">
             {(provided) => (
               <div
@@ -75,7 +121,7 @@ class Game1 extends PureComponent {
                 ref={provided.innerRef}
                 className="icons"
               >
-                {Game1.getIcons(icons)}
+                {Game1.getIcons(icons, 'base')}
                 {provided.placeholder}
               </div>
             )}
