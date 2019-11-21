c3430e5f75fe6d231cb36bccba8d848cf8dd3b22
nail361 <nail361@mail.ru>
2019-11-21 17:47:14 +0300

    errors catch in fetch

--- projects/src/plugins/help.js
+++ projects/src/plugins/help.js
@@ -1,4 +1,3 @@
-
 export const randomArr = (arr) => {
   const randomedArr = arr.sort(() => Math.random() - 0.5);
   return randomedArr;
@@ -6,21 +5,27 @@ export const randomArr = (arr) => {
 
 export const sendCompleteData = async (code) => {
   const url = `http://188.225.46.145/statistic.php?code=${code}`;
-  const response = await fetch(url, {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      // 'Content-Type': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow',
-    referrer: 'no-referrer',
-    body: JSON.stringify(code),
-  });
-  await response();
-  return response.json(); // парсит JSON ответ в Javascript объект
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      mode: 'no-cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow',
+      referrer: 'no-referrer',
+    });
+    await response();
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default randomArr;
